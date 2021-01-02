80956be459f2ca8982d12d24b345946307dd8f38
Buvaneshvaran <buvaneshvaran1068@gmail.com>
2021-01-03 00:07:06 +0530

    correction files

---
+++ README.md
@@ -0,0 +1,2 @@
+# srivenkateswaraphotography
+# srivenkateswaraphotography
---
+++ index.js
@@ -0,0 +1,26 @@
+// var app = document.getElementById('app');
+
+// var typewriter = new Typewriter(app, {
+// 	loop: true
+// });
+
+// typewriter.typeString('Traditional')
+// 	.pauseFor(2500)
+// 	.deleteAll()
+// 	.typeString('HD Video')
+// 	.pauseFor(2400)
+// 	.deleteAll()
+// 	.typeString('Photos in DVD')
+// 	.pauseFor(2400)
+// 	.deleteAll()
+// 	.typeString('Cinematography')
+// 	.pauseFor(2400)
+// 	.deleteAll()
+// 	.typeString('Online e-Album')
+// 	.pauseFor(2400)
+// 	.deleteAll()
+// 	.typeString('Lingual Audio')
+// 	.pauseFor(2400)
+// 	.start();
+AOS.init();
+new WOW().init();
